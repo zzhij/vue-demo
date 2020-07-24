705601446a9c40f0c1c2974056741fa6a6d855c8
zhuzhijie <zhuzhijie@iofedu.com>
2020-07-24 15:50:39 +0800

    完善  添加dep和watcher

--- list/mvvm/mvvm/index.js
+++ list/mvvm/mvvm/index.js
@@ -5,7 +5,7 @@ class MVue {
     this.$options = options
     if (this.$el) {
       // 1. 创建观察者
-      // new Observer(this.$data);
+      new Observer(this.$data);
       // 2. 编译模版
       new Compiler(this.$el, this);
     }
@@ -20,25 +20,37 @@ const compileUtil = {
     }, vm.$data)
   },
   setVal(expr, vm, inputValue) {
-    let exprs = expr.split('.'), len = expr.length;
+    let exprs = expr.split('.'), len = exprs.length;
     exprs.reduce((data, currentVal, idx) => {
       if (idx == len -1) {
-        data[currentVal] = inputValue
+        data[currentVal] = inputValue;
       } else {
-        return data[currentVal]
+        return data[currentVal];
       }
     }, vm.$data)
   },
+  getContent(expr, vm) {
+    // {{person.name}}--{{person.age}}
+    // 防止修改person.name使得所有值全部被替换
+    return expr.replace(/\{\{(.+?)\}\}/g, (...args) => {
+      return this.getValue(args[1], vm);
+    });
+  },
   text(node, expr, vm) {
-    let value
+    let value;
     if (expr.indexOf('{{') != -1) {
       value = expr.replace(/\{\{(.+?)\}\}/g, (...args) => {
         // text的 Watcher应在此绑定，因为是对插值{{}}进行双向绑定
         // Watcher的构造函数的 getOldVal()方法需要接受数据或者对象，而{{person.name}}不能接收
         console.log('121212')
+        console.log(node)
         console.log(args)
+        new Watcher(args[1], vm, () => {
+          this.updater.textUpdater(node, this.getContent(expr, vm));
+        });
         return this.getValue(args[1], vm);
       });
+      console.log(value)
     } else {
       value = this.getValue(expr, vm)
     }
@@ -46,10 +58,15 @@ const compileUtil = {
   },
   model(node, expr, vm) {
     const value = this.getValue(expr, vm);
-    // 
+    // v-model 绑定对应的 watcher 数据驱动视图
+    new Watcher(expr, vm, (newVal) => {
+      this.updater.modelUpdater(node, newVal);
+    })
     // 视图 => 数据 => 视图
     node.addEventListener('input', (e) => {
-      this.setVal(expr, vm, e.target.value)
+      console.log('v-model 数据变化了：' + e.target.value);
+      console.log(this)
+      this.setVal(expr, vm, e.target.value);
     })
     this.updater.modelUpdater(node, value);
   },
@@ -66,10 +83,10 @@ const compileUtil = {
 class Compiler {
   constructor(el, vm) {
     this.el = this.isElementNode(el) ? el : document.querySelector(el)
-    this.vm = vm
+    this.vm = vm;
     // 预编的文档放入文档碎片中
-    const fragments = this.node2fragments(this.el)
-    this.compile(fragments)
+    const fragments = this.node2fragments(this.el);
+    this.compile(fragments);
     this.el.appendChild(fragments);
   }
   compile(fragments) {
@@ -92,6 +109,7 @@ class Compiler {
   compileElement(node) {
     // 解析dom元素
     console.log('编译HTML')
+    console.log(node)
     let attributes = node.attributes;
     // 对于每个属性 进行遍历
     [...attributes].forEach(attr => {
@@ -100,7 +118,9 @@ class Compiler {
         let [, directive] = name.split('-');
         let [compileKey, detailStr] = directive.split(':');
         // 更新数据 数据驱动视图
-        compileUtil[compileKey](node, value, this.vm, detailStr)
+        compileUtil[compileKey](node, value, this.vm, detailStr);
+        // 删除有指令的标签属性 v-text v-html等，普通的value等原生html标签不必删除
+        node.removeAttribute('v-' + directive);
       }
     })
   }
@@ -111,6 +131,9 @@ class Compiler {
   compileText(node) {
     // 解析text
     // 解析文本中的 {{person.name}}
+    console.log('文本节点数据：');
+    console.log(node)
+    console.log(node.textContent)
     const content = node.textContent;
     const reg = /\{\{.+?\}\}/;
     if (reg.test(content)) {
@@ -119,10 +142,10 @@ class Compiler {
   }
   node2fragments(el) {
     // 创建文档碎片
-    const f = document.createDocumentFragment()
-    let firstChild
+    const f = document.createDocumentFragment();
+    let firstChild;
     while (firstChild = el.firstChild) {
-      f.appendChild(firstChild)
+      f.appendChild(firstChild);
     }
     return f
   }
@@ -131,7 +154,7 @@ class Compiler {
     return node.nodeType === 1;
   }
 }
-
+// 观察者
 class Observer {
   constructor(data) {
     this.observe(data)
@@ -146,16 +169,26 @@ class Observer {
   }
   // 通过 Object.defineProperty 对数据进行劫持
   defineReactive(obj, key, value) {
+    // 递归
     this.observe(value);
-    Object.defineProperties(obj, key, {
+    const dep = new Dep();
+    Object.defineProperty(obj, key, {
       enumerable: true,
       configurable: false,
       get() {
+        console.log('获取值：' + value)
+        console.log(value)
+        // 订阅数据变化，汪dep中添加观察者
+        Dep.target && dep.addWatcher(Dep.target);
         return value;
       },
-      set(newVal) {
+      set: (newVal) => {
+        console.log('设置了新值：' + newVal)
         if (value === newVal) return;
+        this.observe(newVal);
         value = newVal;
+        // 通知watcher 数据发生了变化
+        dep.notify();
       }
     })
   }
@@ -163,13 +196,45 @@ class Observer {
 
 class Watcher {
   constructor(expr, vm, cb) {
-
+    // 通过回调函数实现更新的数据通知到视图
+    this.expr = expr;
+    this.vm = vm;
+    this.cb = cb;
+    this.oldVal = this.getOldVal();
+  }
+  // 获取旧数据
+  getOldVal() {
+    console.log('获取了旧数据')
+    // 再利用getValue获取数据调用getter()方法时先把当前观察者挂载
+    Dep.target = this;
+    const oldVal = compileUtil.getValue(this.expr, this.vm);
+    // 挂载完毕之后需要注销  防止重复挂载 (数据一更新就会挂载)
+    Dep.target = null;
+    return oldVal;
+  }
+  // 通过回调函数更新数据
+  update() {
+    const newVal = compileUtil.getValue(this.expr, this.vm);
+    if (newVal !== this.oldVal) {
+      this.cb(newVal)
+    }
   }
 }
 
-// Dep类存储watch对象
+// Dep类存储watch对象 并在数据变化时通知watcher
 class Dep {
   constructor() {
-
+    this.watcherCollector = [];
+  }
+  // 添加watcher
+  addWatcher(watcher) {
+    console.log('添加watch')
+    console.log(watcher)
+    this.watcherCollector.push(watcher);
+  }
+  // 数据变化时通知watcher更新
+  notify() {
+    console.log('通知数据变化了');
+    this.watcherCollector.forEach(w => w.update());
   }
 }
